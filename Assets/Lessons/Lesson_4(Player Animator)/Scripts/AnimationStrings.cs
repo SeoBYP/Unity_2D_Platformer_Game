bde33e9362b8bff65fc1bbf01bf4fcfe6fddc688
SeoBYP <jajazxcv@naver.com>
2023-09-30 23:53:27 +0900

    [SEO] Unity 2D 플랫포머 게임 개발 Lesson 6 ~ 10

--- Assets/Lessons/Lesson_4(Player Animator)/Scripts/AnimationStrings.cs
+++ Assets/Lessons/Lesson_4(Player Animator)/Scripts/AnimationStrings.cs
@@ -2,8 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public  class AnimationStrings
+namespace Lesson_4
 {
-    public static string IsMoving => "isMoving";
-    public static string IsRunning => "isRunning";
+    public class AnimationStrings
+    {
+        public static string IsGrounded => "isGrounded";
+
+        public static string IsMoving => "isMoving";
+        public static string IsRunning => "isRunning";
+        public static string yVelocity=> "yVelocity";
+        public static string Jump => "Jump";
+
+        public static string IsOnWall => "isOnWall";
+        public static string IsOnCeiling => "isOnCeiling";
+        public static string Attack => "Attack";
+        public static string CanMove => "CanMove";
+        public static string HasTarget => "hasTarget";
+        public static string IsAlive => "isAlive";
+    }
+
 }
